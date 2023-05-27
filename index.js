ef2f7d776e5a6adad0205ac445c413b911759e20
SumitEnG <kumarsukdev404@gmail.com>
2023-05-27 08:18:11 +0530

    cors

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require("express-async-errors");
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(express.json());
 require("./startup/db")();
