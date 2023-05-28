4dbadc3f0d3e711c6c702e7b8efb48a22f9342e9
SumitEnG <kumarsukdev404@gmail.com>
2023-05-28 23:30:21 +0530

    .

--- startup/routes.js
+++ startup/routes.js
@@ -1,8 +1,7 @@
-const listRoutes = require("../routes/list");
-
 module.exports = function (app) {
   const userRoutes = require("../routes/user");
   const authRoutes = require("../routes/auth");
+  const listRoutes = require("../routes/list");
   const error = require("../middleware/error");
 
   app.use("/api/register", userRoutes);
