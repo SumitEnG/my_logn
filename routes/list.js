81c98682e54d60f03767d66c48daefc444433574
SumitEnG <kumarsukdev404@gmail.com>
2023-05-31 23:27:19 +0530

    putList

--- routes/list.js
+++ routes/list.js
@@ -12,6 +12,7 @@ listRoutes.post("/", async (req, res) => {
   const result = validateList(req.body);
   if (result.error) {
     res.status(400).send(result.error.details[0].message);
+    return;
   }
 
   const list = new List(_.pick(req.body, ["name", "time"]));
@@ -19,4 +20,28 @@ listRoutes.post("/", async (req, res) => {
   res.send(list);
 });
 
+listRoutes.put("/", async (req, res) => {
+  const result = validateList(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+  }
+
+  let list = await List.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      time: req.body.time,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!list) {
+    res.status(400).send("invalid task");
+    return;
+  }
+
+  res.send(list);
+});
 module.exports = listRoutes;
